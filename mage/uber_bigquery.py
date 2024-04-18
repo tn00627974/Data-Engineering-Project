9516934693c104ea660863437f0477ca6627cc48
Redmoon <139155210+tn00627974@users.noreply.github.com>
2024-04-19 00:10:58 +0800

    Update uber_bigquery.py

--- mage/uber_bigquery.py
+++ mage/uber_bigquery.py
@@ -7,7 +7,6 @@ from os import path
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
-
 @data_exporter
 def export_data_to_big_query(df: DataFrame, **kwargs) -> None:
     """
