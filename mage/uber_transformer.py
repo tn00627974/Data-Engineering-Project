86f2b3b58fdf628e3b006036960c09f02de02dd0
Redmoon <139155210+tn00627974@users.noreply.github.com>
2024-04-19 00:10:49 +0800

    Update uber_transformer.py

--- mage/uber_transformer.py
+++ mage/uber_transformer.py
@@ -4,7 +4,6 @@ if 'transformer' not in globals():
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
-
 @transformer
 def transform(df, *args, **kwargs):
     """
